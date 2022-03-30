f1d6f48b578fb928f6cb583d7f2fe47dd987f0c4
masataka715 <cdehilmnrr@gmail.com>
2022-03-31 08:48:50 +0900

    ブロックの作成

---
+++ blockchain.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+)
+
+type Block struct {
+	nonce        int
+	previousHash string
+	timestamp    int64
+	transactions []string
+}
+
+func NewBlock(nouce int, previousHash string) *Block {
+	b := new(Block) // newでpoint型を明示できる
+	b.timestamp = time.Now().UnixNano()
+	b.nonce = nouce
+	b.previousHash = previousHash
+	return b
+}
+
+func (b *Block) Print() {
+	fmt.Printf("timestamp        %d\n", b.timestamp)
+	fmt.Printf("nonce            %d\n", b.nonce)
+	fmt.Printf("previous_hash    %s\n", b.previousHash)
+	fmt.Printf("transactions     %s\n", b.transactions)
+}
+
+func init() {
+	log.SetPrefix("Blockchain: ")
+}
+
+func main() {
+	b := NewBlock(0, "init hash")
+	b.Print()
+}

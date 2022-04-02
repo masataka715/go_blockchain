4305ffd7d34d78f4f79e708673380dfce6c19fd5
masataka715 <cdehilmnrr@gmail.com>
2022-04-03 07:36:48 +0900

    ブロックのハッシュを求める

--- README.md
+++ README.md
@@ -1,36 +1,36 @@
 ```
 $ go run blockchain.go
 ========================= Chain 0 ========================= 
-timestamp        1648685084837258000
+timestamp        1648938947956161000
 nonce            0
-previous_hash    Init hash
+previous_hash    9c6e7d6e10ac8cd391751b7cb910a18208a49ff9b678e98afcf0f8f1fca57e70
 transactions     []
 *************************
 ========================= Chain 0 ========================= 
-timestamp        1648685084837258000
+timestamp        1648938947956161000
 nonce            0
-previous_hash    Init hash
+previous_hash    9c6e7d6e10ac8cd391751b7cb910a18208a49ff9b678e98afcf0f8f1fca57e70
 transactions     []
 ========================= Chain 1 ========================= 
-timestamp        1648685084837296000
+timestamp        1648938947956225000
 nonce            5
-previous_hash    hash 1
+previous_hash    f50fb6af3b501bd27920a91d084fb947e61664bcc2682e50a3302eb2eb4a04ce
 transactions     []
 *************************
 ========================= Chain 0 ========================= 
-timestamp        1648685084837258000
+timestamp        1648938947956161000
 nonce            0
-previous_hash    Init hash
+previous_hash    9c6e7d6e10ac8cd391751b7cb910a18208a49ff9b678e98afcf0f8f1fca57e70
 transactions     []
 ========================= Chain 1 ========================= 
-timestamp        1648685084837296000
+timestamp        1648938947956225000
 nonce            5
-previous_hash    hash 1
+previous_hash    f50fb6af3b501bd27920a91d084fb947e61664bcc2682e50a3302eb2eb4a04ce
 transactions     []
 ========================= Chain 2 ========================= 
-timestamp        1648685084837310000
+timestamp        1648938947956258000
 nonce            2
-previous_hash    hash 2
+previous_hash    b82a3c492a5d825b0019e7f36336b04ea1c20b518931e2ac052f44160474b266
 transactions     []
 *************************
 ```
--- blockchain.go
+++ blockchain.go
@@ -1,20 +1,23 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/json"
 	"fmt"
 	"log"
 	"strings"
 	"time"
 )
 
+// Block
 type Block struct {
 	nonce        int
-	previousHash string
+	previousHash [sha256.Size]byte
 	timestamp    int64
 	transactions []string
 }
 
-func NewBlock(nouce int, previousHash string) *Block {
+func NewBlock(nouce int, previousHash [sha256.Size]byte) *Block {
 	b := new(Block) // newでpoint型を明示できる
 	b.timestamp = time.Now().UnixNano()
 	b.nonce = nouce
@@ -25,25 +28,52 @@ func NewBlock(nouce int, previousHash string) *Block {
 func (b *Block) Print() {
 	fmt.Printf("timestamp        %d\n", b.timestamp)
 	fmt.Printf("nonce            %d\n", b.nonce)
-	fmt.Printf("previous_hash    %s\n", b.previousHash)
+	fmt.Printf("previous_hash    %x\n", b.previousHash)
 	fmt.Printf("transactions     %s\n", b.transactions)
 }
 
+func (b *Block) Hash() [sha256.Size]byte {
+	m, _ := json.Marshal(b)
+	return sha256.Sum256([]byte(m))
+}
+
+// MarshalJSON
+// Blockのfieldがprivateなので、json化時にpublicにすることが必要
+func (b *Block) MarshalJSON() ([]byte, error) {
+	return json.Marshal(struct {
+		Timestamp    int64             `json:"timestamp"`
+		Nonce        int               `json:"nonce"`
+		PreviousHash [sha256.Size]byte `json:"previous_hash"`
+		Transactions []string          `json:"transactions"`
+	}{
+		Timestamp:    b.timestamp,
+		Nonce:        b.nonce,
+		PreviousHash: b.previousHash,
+		Transactions: b.transactions,
+	})
+}
+
+// Blockchain
 type Blockchain struct {
 	transactionPool []string
 	chain           []*Block
 }
 
 func NewBlockchain() *Blockchain {
+	b := &Block{}
 	bc := new(Blockchain)
-	bc.CreateBlock(0, "Init hash") // 1個目のブロック
+	bc.CreateBlock(0, b.Hash()) // 1個目のブロック
 	return bc
 }
 
-func (bc *Blockchain) CreateBlock(nonce int, previousHash string) *Block {
+func (bc *Blockchain) CreateBlock(nonce int, previousHash [sha256.Size]byte) *Block {
 	b := NewBlock(nonce, previousHash)
 	bc.chain = append(bc.chain, b)
 	return b
+}
+
+func (bc *Blockchain) LastBlock() *Block {
+	return bc.chain[len(bc.chain)-1]
 }
 
 func (bc *Blockchain) Print() {
@@ -62,8 +92,12 @@ func init() {
 func main() {
 	blockChain := NewBlockchain()
 	blockChain.Print()
-	blockChain.CreateBlock(5, "hash 1") // 2個目のブロック
+
+	previousHash := blockChain.LastBlock().Hash()
+	blockChain.CreateBlock(5, previousHash) // 2個目のブロック
 	blockChain.Print()
-	blockChain.CreateBlock(2, "hash 2") // 3個目のブロック
+
+	previousHash = blockChain.LastBlock().Hash()
+	blockChain.CreateBlock(2, previousHash) // 3個目のブロック
 	blockChain.Print()
 }

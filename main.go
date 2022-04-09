491beda447a59d839609e6aed6bcfdb183cfa043
masataka715 <cdehilmnrr@gmail.com>
2022-04-09 16:21:39 +0900

    トランザクションの署名を生成

--- main.go
+++ main.go
@@ -15,4 +15,8 @@ func main() {
 	fmt.Println("PrivateKey: " + w.PrivateKeyStr())
 	fmt.Println("PublicKey: " + w.PublicKeyStr())
 	fmt.Println("BlockchainAddress:  " + w.BlockchainAddress())
+
+	// Bさんに1.0コインを送る
+	t := wallet.NewTransaction(w.PrivateKey(), w.PublicKey(), w.BlockchainAddress(), "B", 1.0)
+	fmt.Printf("Signature %s\n", t.GenerateSignature())
 }
